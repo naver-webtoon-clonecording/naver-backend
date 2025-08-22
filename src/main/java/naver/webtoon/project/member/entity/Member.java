7486365a09feb4440bd1c85a37318559999989d2
JoonwooKim <kkn8010@gmail.com>
2025-08-22 12:32:11 +0900

    feat: 로그인 한 사용자 포인트 충전 메서드 추가

--- src/main/java/naver/webtoon/project/member/entity/Member.java
+++ src/main/java/naver/webtoon/project/member/entity/Member.java
@@ -21,6 +21,9 @@ public class Member extends Timestamped {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private Integer pointAmount;
+
     @Column(name = "cookie_count", nullable = false)
     private Integer cookieCount;
 
@@ -30,5 +33,8 @@ public class Member extends Timestamped {
         this.username = username;
         this.password = password;
         this.cookieCount = cookieCount;
+    }
+    public void chargePoint(Integer amount){
+        this.pointAmount += amount;
     }
 }
