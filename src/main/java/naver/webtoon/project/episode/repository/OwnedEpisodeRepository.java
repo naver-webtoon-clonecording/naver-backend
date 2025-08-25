ffbe03325aed5bdb1a6309cd12fdae341cae9417
JoonwooKim <kkn8010@gmail.com>
2025-08-26 00:10:22 +0900

    feat : 소장 에피소드 Repository 인터페이스 추가, 객체를 MemberId와 EpisodeId로 찾는 메서드 추가

---
+++ src/main/java/naver/webtoon/project/episode/repository/OwnedEpisodeRepository.java
@@ -0,0 +1,14 @@
+package naver.webtoon.project.episode.repository;
+
+import naver.webtoon.project.episode.entity.Episode;
+import naver.webtoon.project.episode.entity.OwnedEpisode;
+import naver.webtoon.project.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OwnedEpisodeRepository extends JpaRepository<OwnedEpisode, Long> {
+    Optional<OwnedEpisode> findByMemberIdAndEpisodeId(Long memberId, Long episodeId);
+
+    boolean existsByMemberAndEpisode(Member member, Episode episode);
+}
