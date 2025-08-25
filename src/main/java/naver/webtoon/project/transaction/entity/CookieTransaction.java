0ff17d17fe904176555af464a9b2a988b6812cec
JoonwooKim <kkn8010@gmail.com>
2025-08-26 00:01:45 +0900

    feat : 에피소드 소장 시 쿠키가 소모될때 CookieTransaction 객체를 만들어주는 메서드 추가

--- src/main/java/naver/webtoon/project/transaction/entity/CookieTransaction.java
+++ src/main/java/naver/webtoon/project/transaction/entity/CookieTransaction.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import naver.webtoon.project.member.entity.Member;
 
+import static naver.webtoon.project.transaction.entity.Type.CONSUME;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,5 +23,17 @@ public class CookieTransaction extends Transaction{
     public CookieTransaction(Type type, Integer amount, Integer balance, Member member, Long id) {
         super(type, amount, balance, member);
         this.id = id;
+    }
+
+    public static CookieTransaction createConsumeCookieTransaction(Member member, int amount) {
+        Type type = CONSUME;
+        Integer balance = member.getCookieCount() - amount;
+
+        return CookieTransaction.builder()
+                .type(type)
+                .amount(amount)
+                .balance(balance)
+                .member(member)
+                .build();
     }
 }
