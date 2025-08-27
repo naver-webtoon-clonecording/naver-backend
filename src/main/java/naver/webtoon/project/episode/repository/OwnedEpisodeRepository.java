994df1366471a48eba699e144a4a5f6c89899a95
JoonwooKim <kkn8010@gmail.com>
2025-08-27 12:59:19 +0900

    feat : ownedEpisodeRepository 회원 id로 소장 에피소드 객체 조회 메서드 기능 추가

--- src/main/java/naver/webtoon/project/episode/repository/OwnedEpisodeRepository.java
+++ src/main/java/naver/webtoon/project/episode/repository/OwnedEpisodeRepository.java
@@ -5,10 +5,13 @@ import naver.webtoon.project.episode.entity.OwnedEpisode;
 import naver.webtoon.project.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OwnedEpisodeRepository extends JpaRepository<OwnedEpisode, Long> {
     Optional<OwnedEpisode> findByMemberIdAndEpisodeId(Long memberId, Long episodeId);
 
     boolean existsByMemberAndEpisode(Member member, Episode episode);
+
+    List<OwnedEpisode> findByMemberId(Long id);
 }
